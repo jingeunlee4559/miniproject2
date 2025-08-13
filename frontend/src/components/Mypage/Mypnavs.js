642313ef02cda86615cf4b99bd3dd6bca3af1db8
wlsrms4559@gmail.com <wlsrms4559@gmail.com>
2025-08-13 13:42:04 +0900

    마이페이지 네비 수정

--- frontend/src/components/Mypage/Mypnavs.js
+++ frontend/src/components/Mypage/Mypnavs.js
@@ -1,23 +1,38 @@
-import React, { useCallback } from 'react'
-import { Nav } from 'react-bootstrap'
-import "../../css/Mypnavs.css"
-import { useNavigate } from 'react-router-dom';
+import React, { useCallback } from 'react';
+import { Nav } from 'react-bootstrap';
+import '../../css/Mypnavs.css';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Mypnavs = () => {
+    const navigate = useNavigate();
+    const location = useLocation(); // 현재 URL 경로 가져오기
 
-  const navigate = useNavigate();
-  const navigateTo = useCallback((path) => navigate(path), [navigate]);
+    const navigateTo = useCallback(
+        (path) => {
+            navigate(path);
+        },
+        [navigate],
+    );
 
     return (
-        <Nav justify variant="tabs"  className='custom-snavbarmy' >
-          <Nav.Item>
-            <Nav.Link className='navlinkmy' onClick={() => navigateTo("/Mypage")}>내정보</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link className='navlinkmy' onClick={() => navigateTo("/Mypage/schedule")}>일정</Nav.Link>
-          </Nav.Item>
+        <Nav
+            justify
+            variant="tabs"
+            className="custom-snavbarmy"
+            activeKey={location.pathname} // 현재 경로에 맞추어 활성 탭 자동 설정
+        >
+            <Nav.Item>
+                <Nav.Link eventKey="/Mypage" onClick={() => navigateTo('/Mypage')} className="navlinkmy">
+                    내정보
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="/Mypage/schedule" onClick={() => navigateTo('/Mypage/schedule')} className="navlinkmy">
+                    일정
+                </Nav.Link>
+            </Nav.Item>
         </Nav>
-      );
-    }
+    );
+};
 
-export default Mypnavs
+export default Mypnavs;
