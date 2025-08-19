4b75fa0eeac50ec58ab3d61adcedbe2d67f6a724
wlsrms4559@gmail.com <wlsrms4559@gmail.com>
2025-08-19 16:06:31 +0900

    풀 받기 위한 커밋

--- frontend/src/components/Navs.js
+++ frontend/src/components/Navs.js
@@ -65,7 +65,7 @@ function Navs() {
                     {' '}
                     {/* Changed maxWidth to 100% */}
                     <Navbar.Brand onClick={() => navigateTo('/')}>
-                        <img src="img/logo/plogo4.png" alt="로고" className="logo5" />
+                        <img src="img/logo/plogo.png" alt="로고" className="logo5" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : true)} />
                     <Navbar.Collapse id="basic-navbar-nav">
